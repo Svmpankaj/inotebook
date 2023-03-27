7fdc4cbab16a46f3b5d692ae7e1b4213004f8720
Svmpankaj <svmpankaj@gmail.com>
2023-03-27 23:58:43 +0530

    storing data on local db

---
+++ backend/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+
+
+// Create a User using: POST "/api/auth/". Doesn't require Auth
+router.get('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body)
+})
+
+module.exports = router;
