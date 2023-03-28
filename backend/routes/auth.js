3d7e2ad1c6b14cb634cd171ab674dc689e055c3f
Svmpankaj <svmpankaj@gmail.com>
2023-03-29 00:35:05 +0530

    createing login end point

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -56,6 +56,44 @@ router.post('/createuser', [
         console.error(error.message);
         res.status(500).send("Some error occured");
     }
+})
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password Cannot be blanck').exists({ min: 5 })
+], async (req, res) => {
+
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ error: "Please try to login with correct credentials" });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!password) {
+            return res.status(404).json({ error: "Please try to login with correct credentials" });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 
 })
 
