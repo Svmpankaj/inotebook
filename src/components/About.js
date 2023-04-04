a13a70b169fba3667eba1a3cf78299c9d1082bd1
Svmpankaj <svmpankaj@gmail.com>
2023-04-04 23:58:19 +0530

    set a uslocation hook at navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,9 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+// import React, { useContext, useEffect } from 'react'
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
     return (
         <div>
-            This is About {a.state.name} and he is in class {a.state.programmer}
+            This is About page
         </div>
     )
 }
