f576e5b69fffd5aa8e4d0cc41d9d58d17c9a32e1
Svmpankaj <svmpankaj@gmail.com>
2023-04-19 23:20:19 +0530

    fixed some bug on login middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,10 +23,9 @@ router.post('/createuser', [
     // Check whether user with this email exists already
     try {
 
-
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return req.status(404).json({ error: "Sorry a user with this email is already exists" })
+            return res.status(404).json({ error: "Sorry a user with this email is already exists" })
         }
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
@@ -37,10 +36,6 @@ router.post('/createuser', [
             email: req.body.email,
             password: secPass,
         })
-
-        // .then(user => res.json(user))
-        //     .catch(err => console.log(err))
-        // res.json({ error: 'Please enter a unique value for email', message: err.message })
 
         const data = {
             user: {
@@ -60,43 +55,55 @@ router.post('/createuser', [
 })
 
 // ROUTE 2:  Authenticate a User using: POST "/api/auth/login". No login required
-router.post('/login', [
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password Cannot be blanck').exists({ min: 5 })
-], async (req, res) => {
+router.post('/login',
+    body('email').isEmail(),
+    body('password').isLength({ min: 3 }),
+    async (req, res) => {
 
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const { email, password } = req.body;
-    try {
-        let user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        let success = false
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log('dhek bhai', res.body, 'abc')
+            return res.status(400).json({ errors: errors.array() });
         }
-
-        const passwordCompare = await bcrypt.compare(password, user.password);
-        if (!passwordCompare) {
-            return res.status(404).json({ error: "Please try to login with correct credentials" });
-        }
-
-        const data = {
-            user: {
-                id: user.id
+        const { email, password } = req.body;
+        // {const email: req.body.email}
+        // const {password: req.body.password}
+        // User.create({
+        //     email: req.body.email,
+        //     password: req.body.password,
+        // })
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                success = false
+                return res.status(400).json({ error: "Please try to login with correct credentials" });
             }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if (!passwordCompare) {
+                success = false
+                return res.status(400).json({ success, error: "Please try to login with correct credentials" });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            success = true;
+            res.json({ success, authtoken })
+
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
         }
 
-        const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({ authtoken })
-
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
-
-})
+    })
 
 // ROUTE 3:  Get loggendin  User Details using: POST "/api/auth/getuser". login required
 router.post('/getuser', fetchuser, async (req, res) => {
