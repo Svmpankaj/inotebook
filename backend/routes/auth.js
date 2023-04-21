29133c774c8f1d629b48fde606e6e9f4deade060
Svmpankaj <svmpankaj@gmail.com>
2023-04-21 23:56:06 +0530

    change on backend auth file

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,70 +10,64 @@ const JWT_SECRET = 'Pankajisagood$boy';
 
 
 // ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
-], async (req, res) => {
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    // Check whether user with this email exists already
-    try {
-
-        let user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(404).json({ error: "Sorry a user with this email is already exists" })
+router.post('/createuser',
+    body('name').isLength({ min: 3 }).withMessage('Enter a valid name'),
+    body('email').isEmail().withMessage('Enter a valid email'),
+    body('password').isLength({ min: 3 }).withMessage('Password must be atleast 5 characters'),
+    async (req, res) => {
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-        const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
-
-        // Create a new
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: secPass,
-        })
-
-        const data = {
-            user: {
-                id: user.id
+        // Check whether user with this email exists already
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(404).json({ error: "Sorry a user with this email is already exists" })
             }
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            // Create a new
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass,
+            })
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authtoken = jwt.sign(data, JWT_SECRET);
+
+            // res.json(user)
+            res.json({ authtoken })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Some error occured");
         }
-
-        const authtoken = jwt.sign(data, JWT_SECRET);
-
-        // res.json(user)
-        res.json({ authtoken })
-
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some error occured");
-    }
-})
+    })
 
 // ROUTE 2:  Authenticate a User using: POST "/api/auth/login". No login required
-router.post('/login',
-    body('email').isEmail(),
-    body('password').isLength({ min: 3 }),
+router.post('/login', [
+    body('email').isEmail().withMessage('Enter a valid email'),
+    body('password').isLength({ min: 3 }).withMessage('Password Cannot be blank'),
+],
     async (req, res) => {
 
         let success = false
         // If there are errors, return Bad request and the errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            console.log('dhek bhai', res.body, 'abc')
             return res.status(400).json({ errors: errors.array() });
         }
         const { email, password } = req.body;
-        // {const email: req.body.email}
-        // const {password: req.body.password}
-        // User.create({
-        //     email: req.body.email,
-        //     password: req.body.password,
-        // })
+
         try {
             let user = await User.findOne({ email });
             if (!user) {
